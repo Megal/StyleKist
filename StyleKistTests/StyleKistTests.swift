bee2b3e3d0d39c77cb6aace9fc47320ff38a2ff7
Svyatoshenko Misha <megal06@nightmail.ru>
2016-10-10 16:00:03 +0300

    Initial Commit

---
+++ StyleKistTests/StyleKistTests.swift
@@ -0,0 +1,36 @@
+//
+//  StyleKistTests.swift
+//  StyleKistTests
+//
+//  Created by Svyatoshenko "Megal" Misha on 2016-10-10.
+//  Copyright © 2016年 Svyatoshenko "Megal" Misha. All rights reserved.
+//
+
+import XCTest
+@testable import StyleKist
+
+class StyleKistTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
